326350b34d3998ceafb7c35d840e4833ef616002
Onestar90 <mrb8215@gmail.com>
2024-05-16 18:06:00 +0900

    Fix: css 수정

--- src/main/java/dao/TourReservationManagementDAO.java
+++ src/main/java/dao/TourReservationManagementDAO.java
@@ -5,6 +5,8 @@ import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import util.DbConnection;
 import vo.SpotListVO;
@@ -127,5 +129,41 @@ public class TourReservationManagementDAO {
 	    }
 	    return isDuplicate;
 	}
+	
+	public List<TourReservationVO> selectAllTourReservationList(String id) throws ClassNotFoundException, SQLException {
+		List<TourReservationVO> list = new ArrayList<TourReservationVO>();
+		TourReservationVO trVO = null;
+		DbConnection dbCon = DbConnection.getInstance();
+		
+		Connection con =null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		
+		try {
+			Class.forName("oracle.jdbc.OracleDriver");
+            con = dbCon.getConn("jdbc/abn");
+
+            String sql = "select * from RESERVATION" ;
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, id);
+            rs=pstmt.executeQuery();
+            while(rs.next()) {
+            	trVO = new TourReservationVO();
+            	trVO.setResv_code(rs.getString("resv_code"));  
+            	trVO.setCrs_code(rs.getString("crs_code"));
+            	trVO.setLogic(rs.getString("logic"));
+            	trVO.setFare(rs.getInt("fare"));
+            	trVO.setPerson(rs.getInt("person"));
+            	trVO.setResv_flag(rs.getInt("resv_flag"));
+            	trVO.setCreate_date(rs.getDate("create_date"));
+            	
+     	
+            	list.add(trVO);
+            }}finally{
+    			dbCon.closeCon(rs, pstmt, con);
+    		}
+			
+    		return list;
+    		}
 
 }
